d2a89df3b598b8beacea6d709691ab203038ce28
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-10-08 10:05:34 +0200

    basic classes

--- src/auth/reloaded/AuthReloaded.java
+++ src/auth/reloaded/AuthReloaded.java
@@ -1,11 +1,18 @@
 package auth.reloaded;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuthReloaded extends JavaPlugin {
+  private ConsoleCommandSender console = Bukkit.getConsoleSender();
+  
   @Override
   public void onEnable() {
+    for (String cmd : Commands.cmds)
+      this.getCommand(cmd).setExecutor(new Commands());
 
+    Bukkit.getServer().getPluginManager().registerEvents(new Events(), this);
   }
 
   @Override
---
+++ src/auth/reloaded/Events.java
@@ -0,0 +1,9 @@
+package auth.reloaded;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+public class Events implements Listener {
+  private final Plugin plugin = AuthReloaded.getPlugin(AuthReloaded.class);
+}
