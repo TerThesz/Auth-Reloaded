f1e4c83249f24bcaed2207d527cd6ccf66c9f6e4
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-10-08 09:55:45 +0200

    init

---
+++ src/auth/reloaded/AuthReloaded.java
@@ -0,0 +1,15 @@
+package auth.reloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuthReloaded extends JavaPlugin {
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
