e61896a23357c2d0200fefb28232e4ff5567e059
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-10-08 11:05:27 +0200

    mysql

--- src/auth/reloaded/AuthReloaded.java
+++ src/auth/reloaded/AuthReloaded.java
@@ -1,22 +1,42 @@
 package auth.reloaded;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuthReloaded extends JavaPlugin {
   private ConsoleCommandSender console = Bukkit.getConsoleSender();
-  
+  public static MySql mysql;
+
   @Override
   public void onEnable() {
+    console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] Enabling plugin.");
     for (String cmd : Commands.cmds)
       this.getCommand(cmd).setExecutor(new Commands());
 
     Bukkit.getServer().getPluginManager().registerEvents(new Events(), this);
+
+    getConfig().options().copyDefaults(true);
+    saveDefaultConfig();
+    console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] Plugin setup finished.");
+
+    mysql.start();
+    if (mysql.isConnected())
+      console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] MySQL connected.");
+
+    console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] Plugin is enabled.");
   }
 
   @Override
   public void onDisable() {
+    console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] Disabling plugin.");
+    saveDefaultConfig();
 
+    mysql.end();
+    if (!mysql.isConnected())
+      console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] MySQL disconnected.");
+
+    console.sendMessage(ChatColor.GREEN + "[Auth-Reloaded] Plugin is disabled.");
   }
 }
---
+++ src/auth/reloaded/MySql.java
@@ -0,0 +1,57 @@
+package auth.reloaded;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.Plugin;
+
+public class MySql {
+  private Connection connection;
+  private String username, host, database, password, table;
+  private Integer port;
+
+  private Plugin plugin = AuthReloaded.getPlugin(AuthReloaded.class);
+  private Configuration config = plugin.getConfig();
+
+  public void start() {
+    username = config.getString("username");
+    password = config.getString("password");
+    database = config.getString("database");
+    host = config.getString("host");
+    table = config.getString("table");
+    port = config.getInt("port");
+
+    try {
+      connect();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+  }
+
+  private void connect() throws SQLException, ClassNotFoundException {
+    Class.forName("com.mysql.jdbc.Driver");
+    connection = DriverManager.getConnection("jdbc:mysql://" + host+ ":" + port + "/" + database, username, password);
+  }
+
+  public void end() {
+    if (isConnected()) {
+      try {
+        connection.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public Connection getConnection() {
+    return connection;
+  }
+
+  public Boolean isConnected() {
+    return (connection == null ? false : true);
+  }
+}
